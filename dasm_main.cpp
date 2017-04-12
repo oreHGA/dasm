42c9237e6ead7bd22ef0aeabb8892257eb9ea0ef
oreHGA <oreogundipe@gmail.com>
2017-04-12 04:19:23 -0700

    successfully able to get the opcode for each set of instructions

--- dasm_main.cpp
+++ dasm_main.cpp
@@ -5,6 +5,71 @@
 #include <sstream>
 using namespace std;
 
+int char_to_int(char val){
+    if(val == '1')
+        return 1;
+    else if( val == '2')
+        return 2;
+    else if( val == '3')
+        return 3;
+    else if( val == '4')
+        return 4;
+    else if( val == '5')
+        return 5;
+    else if( val == '6')
+        return 6;
+    else if( val == '7')
+        return 7;
+    else if( val == '8')
+        return 8;
+    else if( val == '9')
+        return 9;
+    else if( val == 'A')
+        return 10;
+    else if( val == 'B')
+        return 11;
+    else if( val == 'C')
+        return 12;
+    else if( val == 'D')
+        return 13;
+    else if( val == 'E')
+        return 14;
+    else if( val == 'F')
+        return 15;    
+}
+char int_to_char(int val){
+    if(val == 1)
+        return '1';
+    else if( val == 2)
+        return '2';
+    else if( val == 3)
+        return '3';
+    else if( val == 4)
+        return '4';
+    else if( val == 5)
+        return '5';
+    else if( val == 6)
+        return '6';
+    else if( val == 7)
+        return '7';
+    else if( val == 8)
+        return '8';
+    else if( val == 9)
+        return '9';
+    else if( val == 10)
+        return 'A';
+    else if( val == 11)
+        return 'B';
+    else if( val == 12)
+        return 'C';
+    else if( val == 13)
+        return 'D';
+    else if( val == 14)
+        return 'E';
+    else if( val == 15)
+        return 'F';    
+}
+
 int main(int argc,char *argv[]){
     if(argc != 2){
         //throw an error if the program is not executed properly throw an error
@@ -24,9 +89,9 @@ int main(int argc,char *argv[]){
         ifstream symtab(sym.c_str()); // symtab ready 
 
         // we check if we can open the files
-
         if( !symtab.is_open() || !object_file.is_open() ){
-            cout<<"usage: "<<argv[0]<<" Files necessary for execution are not present\n"<<"Ensure you have <filename>.sym and <filename>.obj ready\n";
+            cout<<"usage: "<<argv[0]<<" Files necessary for execution are not present\n";
+            cout<<"Ensure you have <filename>.sym and <filename>.obj ready\n";
             exit(1); // 1 means that something abruptly went wrong
         }
         else{
@@ -34,24 +99,61 @@ int main(int argc,char *argv[]){
             string line;
             ofstream result_file;
             while(getline(object_file,line)){
-                cout<<line<< "\n";
+                // cout<<line<< "\n";
                 // now we gonna process each line one by one 
                 int length = line.size();
                 int i = 0;
                 while (i<length){
                     if(line.at(i) == 'H' ){
                         // we are in the header record now
+                        // 
+                        // cout<<"Header\n";
                         // next thing is to read the next 6 characters
-                        cout<<"Header\n";
+                        string programname = line.substr(i+1,6) ;
+                        cout<<programname<<"\n"; // eventually we will have to write the name into the new file we are trying to create
+                        //now continue
+                        break;
+
                     }
                     else if(line.at(i) == 'T'){
-                        cout<<"Text\n";
+                        // cout<<"Text\n";
+                        // now for the text record.. this is where things actually get interesting because we want taking in strings we 
+                        // like without actually specifying values rather using a counter
+                        // ah ah .. what we do in this case is that we should basically have a new loop and get the  
+                        int index = i + 6;
+                        while(true){
+                            //read the first two 
+                            
+                            string first_two = line.substr(index+1,2);
+                            int temp_val = char_to_int( first_two[1] );
+                            //we need to convert the char value to its hexadecimal representation
+                            // then perform the bit modification stuff to get the n,i values and also get the opcode
+                            int n_i = temp_val & ( (2 << 1) - 1 );  // so now we have our N AND I values from here
+                            bool n_flag = false;
+                            bool i_flag = false;
+                            // performing some bit shifting to get the N AND 1 values from variable n_i
+                            if( (n_i & 1) == 1 ){
+                                i_flag = true;
+                            }   
+                            else if( (n_i >> 1) == 1){
+                                n_flag = true; 
+                            }    
+                            // we now have our n and i flag    
+                            temp_val = temp_val - n_i ;// this is the second part of the opcode
+                            // we need to get our opcode from variable first_two
+                            stringstream ss;
+                            string opcode;
+                            ss << first_two[0] << int_to_char(temp_val);
+                            ss >> opcode;
+                            cout<< opcode<<"\n";
+                            break;
+                        }
                     }
                     else if(line.at(i) == 'M'){
                         break;
                     }
                     else if(line.at(i) == 'E'){
-                        cout<<"End\n";
+                        // cout<<"End\n";
                     }
                     break;
                 }
@@ -65,6 +167,6 @@ int main(int argc,char *argv[]){
             // resultfile.close();
         }   
     }
-    cout<<"Welcome back Captiain!I missed you\n";
+    // cout<<"Welcome back Captiain!I missed you\n";
     return 0;
 }
