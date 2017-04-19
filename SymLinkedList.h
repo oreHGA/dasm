e4280e3b1b6cec961aeec9e63197d03c89760a47
ayessamedrano <ayessamedrano@yahoo.com>
2017-04-18 18:42:26 -0700

    Update SymLinkedList.h

--- SymLinkedList.h
+++ SymLinkedList.h
@@ -5,8 +5,9 @@
 
 using namespace std;
 
-void initNode(struct Node *head, string s, string a);
-Node* addNode(struct Node *head, string s, string a);
+void initNode(struct Node *head, string s, string a, string f);
+Node* addNode(struct Node *head, string s, string a, string f);
 Node* parseSym(string fileName);
 string getAddress(Node *head, string s);
 string getSymbol(Node *head, string a);
+string getFlag(Node *head, string s); 
